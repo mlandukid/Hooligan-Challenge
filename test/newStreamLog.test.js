3377fdfca2c05b570d2fde47f491330165e98c7c
your_github_username <loyisom@navigare.co.za>
2022-08-31 13:21:15 +0200

    test run but can only pass 3 times before failure

--- test/newStreamLog.test.js
+++ test/newStreamLog.test.js
@@ -1,8 +1,19 @@
 const axios = require("axios");
 describe("logStream()", () => {
-    it("returns 201", () => {
-      axios.post("https://h173bq3s8k.execute-api.us-east-1.amazonaws.com/dev/stream", {
-        userId: "12345",
+    it("logs a new stream to the stream log", async () => {
+        const post = () => {
+          return axios.post(
+            "https://h173bq3s8k.execute-api.us-east-1.amazonaws.com/dev/stream",
+            {
+              userId: "1",
+            }
+          );
+        };
+    
+        await post().then((response) => {
+          expect(response.data.statusCode).toEqual(201);
+          expect(response.data.message.userId).toEqual("1");
+          expect(response.data.message.streamId).toBeDefined();
     });
 });
 });
